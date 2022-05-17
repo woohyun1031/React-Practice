a7851fa6f7669046f18fa1799e4631ff4c59598b
Woohyun <ktkwhms3@gmail.com>
2022-05-18 00:39:53 +0900

    Refactor: sumOf code

--- testing_practice/sum.js
+++ testing_practice/sum.js
@@ -3,11 +3,7 @@ function sum(a, b) {
 }
 
 function sumOf(numbers) {
-  let result = 0;
-  numbers.forEach((n) => {
-    result += n;
-  });
-  return result;
+  return numbers.reduce((acc, current) => acc + current, 0);
 }
 
 exports.sum = sum;
