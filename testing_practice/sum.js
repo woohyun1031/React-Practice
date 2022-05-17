3bab7ac82bf29a15ab7d4f984273a623e0acbda4
Woohyun <ktkwhms3@gmail.com>
2022-05-18 00:39:00 +0900

    Feat: describe sum

--- testing_practice/sum.js
+++ testing_practice/sum.js
@@ -2,4 +2,13 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum; // 내보내기
+function sumOf(numbers) {
+  let result = 0;
+  numbers.forEach((n) => {
+    result += n;
+  });
+  return result;
+}
+
+exports.sum = sum;
+exports.sumOf = sumOf;
--- testing_practice/sum.test.js
+++ testing_practice/sum.test.js
@@ -1,7 +1,14 @@
-const sum = require("./sum");
+const { sum, sumOf } = require("./sum");
 
-it("calculates 1 + 2", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum func", () => {
+  it("calculates 1 + 2", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it("calculates all numbers", () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(sumOf(array)).toBe(15);
+  });
 });
 
 //test : 새로운 테스트 케이스를 만드는 함수
@@ -10,3 +17,5 @@ it("calculates 1 + 2", () => {
 
 //toBe (matchers 함수) : 특정 값이 어떤 조건을 만족하는지, 또는 어떤 함수가 실행이 됐는지, 에러가 났는지 등을 확인 할 수 있게 해줍니다.
 //여기서 toBe 는 특정 값이 우리가 정한 값과 일치하는지 확인을 해줍니다.
+
+//describe : 여러 테스트 케이스를 묶을 수 있다.
