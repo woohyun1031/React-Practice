e837ecc23c148cf3e85180f71137c5deeb7d1675
Woohyun <ktkwhms3@gmail.com>
2022-05-18 01:23:41 +0900

    Refactor: TDD practice
    Refactor: TDD practice,
    Feat: min,avg test case

--- testing_practice/tdd_test/stats.js
+++ testing_practice/tdd_test/stats.js
@@ -1,11 +1,18 @@
 const max = (numbers) => {
-  let result = numbers[0];
-  numbers.forEach((n) => {
-    if (n > result) {
-      result = n;
-    }
-  });
+  const result = Math.max(...numbers);
   return result;
 };
 
+const min = (numbers) => {
+  const result = Math.min(...numbers);
+  return result;
+};
+
+const avg = (numbers) => {
+  const sum = numbers.reduce((prev, current) => prev + current, 0);
+  return sum / numbers.length;
+};
+
 exports.max = max;
+exports.min = min;
+exports.avg = avg;
--- testing_practice/tdd_test/stats.test.js
+++ testing_practice/tdd_test/stats.test.js
@@ -4,4 +4,10 @@ describe("stats_test", () => {
   it("get maximum value", () => {
     expect(stats.max([1, 2, 3, 4])).toBe(4);
   });
+  it("get minimum value", () => {
+    expect(stats.min([1, 2, 3, 4])).toBe(1);
+  });
+  it("get average value", () => {
+    expect(stats.avg([1, 2, 3, 4, 5])).toBe(3);
+  });
 });
