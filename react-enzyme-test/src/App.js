7bd4d1379bcfefa1b3f00ce546568433ff00fbc2
Woohyun <ktkwhms3@gmail.com>
2022-05-18 17:59:44 +0900

    Initialize react enzyme practice

---
+++ react-enzyme-test/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <Profile username="velopert" name="김민준" />
+    </div>
+  );
+}
+
+export default App;
