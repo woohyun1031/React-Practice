68bab194df1caf8a16ea375e765c61abed5d5163
Woohyun <ktkwhms3@gmail.com>
2022-05-18 15:50:42 +0900

    Feat: TDD test, median, mode

--- testing_practice/tdd_test/stats.js
+++ testing_practice/tdd_test/stats.js
@@ -8,11 +8,49 @@ const min = (numbers) => {
   return result;
 };
 
-const avg = (numbers) => {
-  const sum = numbers.reduce((prev, current) => prev + current, 0);
-  return sum / numbers.length;
+const avg = (numbers) =>
+  numbers.reduce(
+    (acc, current, index, { length }) => acc + current / length,
+    0
+  );
+
+const sort = (numbers) => numbers.sort((a, b) => a - b);
+
+const median = (numbers) => {
+  const { length } = numbers;
+  const middle = Math.floor(length / 2);
+  return length % 2
+    ? numbers[middle]
+    : (numbers[middle - 1] + numbers[middle]) / 2;
+};
+
+const mode = (numbers) => {
+  const counts = numbers.reduce(
+    (acc, current) => acc.set(current, acc.get(current) + 1 || 1),
+    new Map()
+  );
+
+  const maxCount = Math.max(...counts.values());
+  const modes = [...counts.keys()].filter(
+    (number) => counts.get(number) === maxCount
+  );
+
+  if (modes.length === numbers.length) {
+    // 최빈값이 없음
+    return null;
+  }
+
+  if (modes.length > 1) {
+    // 최빈값이 여러개
+    return modes;
+  }
+  // 최빈값이 하나
+  return modes[0];
 };
 
 exports.max = max;
 exports.min = min;
 exports.avg = avg;
+exports.sort = sort;
+exports.median = median;
+exports.mode = mode;
