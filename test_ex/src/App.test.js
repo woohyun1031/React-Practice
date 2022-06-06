e5de2ce28740c64833436318ef561e6f138d4272
Woohyun <ktkwhms3@gmail.com>
2022-06-06 20:52:53 +0900

    Test: React-testing-library, snapshot test

--- test_ex/src/App.test.js
+++ test_ex/src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import add from "./calculator";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("add test", () => {
+  expect(add(1, 2)).toBe(3);
 });
---
+++ test_ex/src/Dog.test.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Dog", () => {
+  test("dog page 잘 뜨나?", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    ).debug(); // .debug()를 붙이면 터미널에서 스냅샷을 볼 수 있어요!
+  });
+});
